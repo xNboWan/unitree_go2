9922b324438c647797ac8c63590792a2dc89ff59
NboWan <aa01082241015@gmail.com>
2026-04-26 14:38:19 +0800

    add go2 env config from isaaclab

--- source/unitree_go2/unitree_go2/tasks/manager_based/unitree_go2/__init__.py
+++ source/unitree_go2/unitree_go2/tasks/manager_based/unitree_go2/__init__.py
@@ -13,13 +13,41 @@ from . import agents
 
 
 gym.register(
-    id="Template-Unitree-Go2-v0",
+    id="Template-Unitree-Go2-Flat-v0",
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": f"{__name__}.unitree_go2_env_cfg:Go2EnvCfg",
-        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:PPORunnerCfg",
+        "env_cfg_entry_point": f"{__name__}.unitree_go2_flat_env_cfg:UnitreeGo2FlatEnvCfg",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:Go2FlatPPORunnerCfg",
     },
+)
 
-    
+gym.register(
+    id="Template-Unitree-Go2-Rough-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.unitree_go2_rough_env_cfg:UnitreeGo2RoughEnvCfg",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:Go2RoughPPORunnerCfg",
+    },
+)
+
+gym.register(
+    id="Template-Unitree-Go2-Flat-v0-PLAY",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.unitree_go2_flat_env_cfg:UnitreeGo2FlatEnvCfg_PLAY",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:Go2FlatPPORunnerCfg",
+    },
+)
+
+gym.register(
+    id="Template-Unitree-Go2-Rough-v0-PLAY",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.unitree_go2_rough_env_cfg:UnitreeGo2RoughEnvCfg_PLAY",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:Go2RoughPPORunnerCfg",
+    },
 )
